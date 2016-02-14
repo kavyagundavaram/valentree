410a4a0badd60cc6c3f0394e74f515f74819b8f7
Varun Ved <varunmved@gmail.com>
2016-02-13 18:10:32 -0800

    nothing works

--- hello.py
+++ hello.py
@@ -1,9 +1,31 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Here we use a class of some kind to represent and validate our
+    # client-side form data. For example, WTForms is a library that will
+    # handle this for us, and we use a custom LoginForm to validate.
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        login_user(user)
+
+        flask.flash('Logged in successfully.')
+
+        next = flask.request.args.get('next')
+        # next_is_valid should check if the user has valid
+        # permission to access the `next` url
+        if not next_is_valid(next):
+            return flask.abort(400)
+
+        return flask.redirect(next or flask.url_for('index'))
+    return flask.render_template('login.html', form=form)
+
 @app.route('/')
 def index():
-    return render_template('hello.html')
+    return "ayy lmao"
 
 if __name__ == '__main__':
     app.run()
