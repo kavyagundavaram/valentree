2e06b3fe3e4e7c1cb99e45c0ee92593a184d523e
Jeffrey Shaw <jxjshaw@gmail.com>
2016-02-13 22:56:07 -0800

    Uploaded current design assets
    work in progress
    
    - next up adding transition for envelope
    - also readability/sizing stuffffff

---
+++ designAssets/eyy.js
@@ -0,0 +1,25 @@
+$(".envelope").hover(function() { 
+    $('.paper').css('top', '-99px');
+});
+
+$(".envelope").mouseout(function() { 
+    $('.paper').css('top', '-0px');
+});
+
+
+
+
+// $(".paper").click(function() {
+//    $(this).toggleClass("hilite"); {
+     
+//       $('.paper').css('top', '-0px'); 
+//    }
+   
+// });
+
+
+
+
+// if ($('.paper').css('top') === '-0px') {
+//     $('.paper').css('top', '-99px');
+// }
